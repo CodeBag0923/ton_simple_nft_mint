df5fd262f0e04966c9cc222b7ad2cbb29694a124
CodeBag0923 <codemaven0923@gmail.com>
2024-08-01 08:37:43 -0300

    complete nft minting

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import * as dotenv from "dotenv";
 
-import { openWallet } from "./utils";
+import { toNano } from "ton-core";
 import { readdir } from "fs/promises";
+
+import { openWallet } from "./utils";
+import { waitSeqno } from "./delay";
+import { NftCollection } from "./contracts/NftCollection";
+import { NftItem } from "./contracts/NftItem";
 import { updateMetadataFiles, uploadFolderToIPFS } from "./metadata";
+import { GetGemsSaleData, NftSale } from "./contracts/NftSale";
+import { NftMarketplace } from "./contracts/NftMarketplace";
 
 dotenv.config();
 
@@ -24,6 +31,70 @@ async function init() {
   console.log(
     `Successfully uploaded the metadata to ipfs: https://gateway.pinata.cloud/ipfs/${metadataIpfsHash}`
   );
+
+  console.log("Start deploy of nft collection...");
+  const collectionData = {
+    ownerAddress: wallet.contract.address,
+    royaltyPercent: 0.05, // 0.05 = 5%
+    royaltyAddress: wallet.contract.address,
+    nextItemIndex: 0,
+    collectionContentUrl: `ipfs://${metadataIpfsHash}/collection.json`,
+    commonContentUrl: `ipfs://${metadataIpfsHash}/`,
+  };
+  const collection = new NftCollection(collectionData);
+  let seqno = await collection.deploy(wallet);
+  console.log(`Collection deployed: ${collection.address}`);
+  await waitSeqno(seqno, wallet);
+
+  // Deploy nft items
+  const files = await readdir(metadataFolderPath);
+  files.pop();
+  let index = 0;
+
+  seqno = await collection.topUpBalance(wallet, files.length);
+  await waitSeqno(seqno, wallet);
+  console.log(`Balance top-upped`);
+
+  for (const file of files) {
+    console.log(`Start deploy of ${index + 1} NFT`);
+    const mintParams = {
+      queryId: 0,
+      itemOwnerAddress: wallet.contract.address,
+      itemIndex: index,
+      amount: toNano("0.05"),
+      commonContentUrl: file,
+    };
+    const nftItem = new NftItem(collection);
+    seqno = await nftItem.deploy(wallet, mintParams);
+    console.log(`Successfully deployed ${index + 1} NFT`);
+    await waitSeqno(seqno, wallet);
+    index++;
+  }
+
+  console.log("Start deploy of new marketplace  ");
+  const marketplace = new NftMarketplace(wallet.contract.address);
+  seqno = await marketplace.deploy(wallet);
+  await waitSeqno(seqno, wallet);
+  console.log("Successfully deployed new marketplace");
+
+  const nftToSaleAddress = await NftItem.getAddressByIndex(collection.address, 0);
+  const saleData: GetGemsSaleData = {
+    isComplete: false,
+    createdAt: Math.ceil(Date.now() / 1000),
+    marketplaceAddress: marketplace.address,
+    nftAddress: nftToSaleAddress,
+    nftOwnerAddress: null,
+    fullPrice: toNano("10"),
+    marketplaceFeeAddress: wallet.contract.address,
+    marketplaceFee: toNano("1"),
+    royaltyAddress: wallet.contract.address,
+    royaltyAmount: toNano("0.5"),
+  };
+  const nftSaleContract = new NftSale(saleData);
+  seqno = await nftSaleContract.deploy(wallet);
+  await waitSeqno(seqno, wallet);
+
+  await NftItem.transfer(wallet, nftToSaleAddress, nftSaleContract.address);
 }
 
 void init();
--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,7 @@ import {
   Cell,
   OpenedContract,
   TonClient,
+  WalletContractV3R2,
   WalletContractV4,
 } from "ton";
 
@@ -14,17 +15,65 @@ export type OpenedWallet = {
 
 export async function openWallet(mnemonic: string[], testnet: boolean) {
   const keyPair = await mnemonicToPrivateKey(mnemonic);
-  const toncenterBaseEndpoiint = testnet ? "https://testnet.toncenter.com" : "https://toncenter.com";
+
+  const toncenterBaseEndpoint: string = testnet
+    ? "https://testnet.toncenter.com"
+    : "https://toncenter.com";
+
   const client = new TonClient({
-    endpoint: `${toncenterBaseEndpoiint}/api/v2/jsonRPC`,
-    apiKey: process.env.TONCENTER_API_KEY
+    endpoint: `${toncenterBaseEndpoint}/api/v2/jsonRPC`,
+    apiKey: process.env.TONCENTER_API_KEY,
   });
+
   const wallet = WalletContractV4.create({
     workchain: 0,
-    publicKey: keyPair.publicKey
-  })
+    publicKey: keyPair.publicKey,
+  });
 
-  const contract = client.open(wallet);
-  return {contract, keyPair}
+  let contract = client.open(wallet);
+  return { contract, keyPair };
 }
 
+function bufferToChunks(buff: Buffer, chunkSize: number) {
+  const chunks: Buffer[] = [];
+  while (buff.byteLength > 0) {
+    chunks.push(buff.slice(0, chunkSize));
+    buff = buff.slice(chunkSize);
+  }
+  return chunks;
+}
+
+function makeSnakeCell(data: Buffer): Cell {
+  const chunks = bufferToChunks(data, 127);
+
+  if (chunks.length === 0) {
+    return beginCell().endCell();
+  }
+
+  if (chunks.length === 1) {
+    return beginCell().storeBuffer(chunks[0]).endCell();
+  }
+
+  let curCell = beginCell();
+
+  for (let i = chunks.length - 1; i >= 0; i--) {
+    const chunk = chunks[i];
+
+    curCell.storeBuffer(chunk);
+
+    if (i - 1 >= 0) {
+      const nextCell = beginCell();
+      nextCell.storeRef(curCell);
+      curCell = nextCell;
+    }
+  }
+
+  return curCell.endCell();
+}
+
+export function encodeOffChainContent(content: string) {
+  let data = Buffer.from(content);
+  const offChainPrefix = Buffer.from([0x01]);
+  data = Buffer.concat([offChainPrefix, data]);
+  return makeSnakeCell(data);
+}
